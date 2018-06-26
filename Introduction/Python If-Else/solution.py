8ade77a14488b13ea5ced527973e01387221cd8e
jhonatancasale <casale.jhon@gmail.com>
2018-06-26 06:41:43 -0300

    Add solution for `Python If-Else`

---
+++ Introduction/Python If-Else/solution.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    if n % 2:
+        print("Weird")
+    else:
+        if n in range(2, 5+1):
+            print("Not Weird")
+        elif n in range(6, 20+1):
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
